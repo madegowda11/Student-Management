396a0397558726ccee872c5063b5bf1abeaf8509
Maadhu <140439336+madegowda11@users.noreply.github.com>
2026-06-28 20:28:33 +0530

    Create Student DTO class with relevant fields
    This class serves as a data transfer object for student information, including fields for student ID, name, phone, email, branch, location, password, and date.

---
+++ studentapp.dto/Student.java
@@ -0,0 +1,68 @@
+package com.dcl.studentapp.dto;
+//pojo class
+public class Student {
+
+	private int sid;
+	private String sname;
+	private long phone;
+	private String email;
+	private String branch;
+	private String location;
+	private String password;
+	private String date;
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + ", phone=" + phone + ", email=" + email + ", branch="
+				+ branch + ", location=" + location + ", date=" + date + "]";
+	}
+	
+	
+}
